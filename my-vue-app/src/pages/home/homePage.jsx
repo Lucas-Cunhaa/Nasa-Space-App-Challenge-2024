51bc5b0051cdfaa30911ef99ba6cd201056680b3
lucas-cunha <lucasgalvao260@gmail.com>
2024-10-05 11:16:12 -0300

    feat: added all the pages and the react router doom

--- my-vue-app/src/pages/home/homePage.jsx
+++ my-vue-app/src/pages/home/homePage.jsx
@@ -2,7 +2,14 @@ const HomePage = () => {
     return (
         <>
             <h1 className="home-title" >MY EXOPLANETS</h1>
+            <h2 className="home-subtitle"> What it is an exoplanet ?</h2>
             <div className="home-content">
+                <div className="planet-infos">
+                    <h2 className="about"></h2>
+                    <div className="planet">
+                        <h3 className="planet-name"></h3>
+                    </div>
+                </div>
                 <div className="create-planet">
                     <button className="create-button"></button>
                 </div>
