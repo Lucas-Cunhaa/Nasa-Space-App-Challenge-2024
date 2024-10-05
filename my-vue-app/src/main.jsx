e96e9ac03a5209ada3539eadf3f9a84f610bb55d
lucas-cunha <lucasgalvao260@gmail.com>
2024-10-05 10:44:40 -0300

    feat: added the home page

--- my-vue-app/src/main.jsx
+++ my-vue-app/src/main.jsx
@@ -1,8 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
 import './index.css'
+import HomePage from './pages/home/homePage.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+const router = createBrowserRouter([
+  {
+    path: "/", 
+    element: <HomePage />, 
+    
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
---
+++ my-vue-app/src/pages/home/homePage.jsx
@@ -0,0 +1,17 @@
+const HomePage = () => {
+    return (
+        <>
+            <h1 className="home-title" >MY EXOPLANETS</h1>
+            <div className="home-content">
+                <div className="create-planet">
+                    <button className="create-button"></button>
+                </div>
+                <div className="discovery-more">
+                <button className="discovery-button"></button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default HomePage
