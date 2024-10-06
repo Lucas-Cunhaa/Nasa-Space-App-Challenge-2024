9f4eea5fd984f5f06386dc602d22a145542f8d1b
lucas-cunha <lucasgalvao260@gmail.com>
2024-10-06 11:24:08 -0300

    feat: added the form for create the user exoplanet and the paint

---
+++ my-vue-app/src/components/CreatePlanetForm.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import "../css/form.css"
+import { useForm } from 'react-hook-form';
+
+const CreatePlanetForm = () => {
+        const { register, handleSubmit, formState: { errors } } = useForm();
+    
+        const onSubmit = (data) => {
+            console.log(data); 
+        };
+
+        const stars = [
+            "Proxima Centauri",
+            "TRAPPIST - 1",
+            "Kepler - 186",
+            "Pegasi",
+            "Tau Ceti", 
+            "GSC 02620-00648"
+        ]
+    
+        return (
+            <div className='form'>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className='form-group'>
+                    <label>Name:</label>
+                    <input 
+                        type="text" 
+                        {...register('name', { required: 'Name is required' })} 
+                    />
+                    {errors.name && <p>{errors.name.message}</p>}
+                </div>
+    
+                <div className='form-group'>
+                    <label>Star:</label>
+                    <input 
+                        type="text" 
+                        {...register('star', { required: 'Star is required' })} 
+                    />
+                    {errors.star && <p>{errors.star.message}</p>}
+                </div>
+    
+                <div className='form-group'>
+                    <label>Size(radio KM):</label>
+                    <div> 
+                            <input 
+                                type="number" 
+                                {...register('size', { required: 'Size is required' })} 
+                            />
+                    </div>
+                    {errors.size && <p>{errors.size.message}</p>}
+                </div>
+    
+                <div>
+                    <label>Mass:</label>
+                    <input 
+                        type="number" 
+                        {...register('mass', { required: 'Mass is required' })} 
+                    />
+                    {errors.mass && <p>{errors.mass.message}</p>}
+                </div>
+    
+                <button className ="link-button" type="submit">Submit</button>
+            </form>
+         </div>
+        );
+    }; 
+
+export default CreatePlanetForm
